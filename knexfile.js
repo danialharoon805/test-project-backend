ef485ce14a2e1ad4a11581d946287ab1cd5d430c
danial <danialharoon805@gmail.com>
2021-10-28 13:31:37 +0500

    change database to postgresql

--- knexfile.js
+++ knexfile.js
@@ -2,18 +2,17 @@ const config = require('config');
 
 module.exports = {
   client: config.database.client,
-  // connection:
-  //   "mysql://root:SdCho2fs7xX7JerkN9LP@zuno-db-01.csm3un4ykbsi.us-east-1.rds.amazonaws.com:3306/zuno",
   connection: {
-      database: config.database.connection.database,
-      user: config.database.connection.user,
-      password: config.database.connection.password
+    host: config.database.connection.host,
+    user: config.database.connection.user,
+    password: config.database.connection.password,
+    database: config.database.connection.database
   },
   pool: {
-      min: config.database.pool.min,
-      max: config.database.pool.max
+    min: config.database.pool.min,
+    max: config.database.pool.max
   },
   migrations: {
-      tableName: config.database.migrations.tableName
+    tableName: config.database.migrations.tableName
   }
 };
